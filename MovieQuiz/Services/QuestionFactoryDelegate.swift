10cacfb0e6b7722c9b864ac6376a6e09ca35228d
Ilya Valito <82824022+DmnUAll@users.noreply.github.com>
2022-10-15 17:05:33 +0300

    Excluded the mock data usage from the project. All questions are now loading, using  IMDb API.

--- MovieQuiz/Services/QuestionFactoryDelegate.swift
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
-protocol QuestionFactoryDelegate: AnyObject {   // Using 'class' keyword to define a class-constrained protocol is deprecated; use 'AnyObject' instead
+protocol QuestionFactoryDelegate: AnyObject {
     func didReceiveNextQuestion(question: QuizQuestion?)
+    func didLoadDataFromServer()
+    func didFailToLoadData(with error: Error)
 }
